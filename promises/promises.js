d728d6d3eb99d56372aa91e731e48e70548a6d01
Jack Peterson <jlpeterson829@gmail.com>
2018-10-17 09:16:53 -0600

    Update promises file to include async await

--- promises/promises.js
+++ promises/promises.js
@@ -1,37 +1,70 @@
+// Async code
+// ==========
+
 setTimeout(() => {
-  console.log('hello')
+  // console.log('hello')
   // throw Error
 }, 0)
 
-console.log('goodbye')
+// console.log('goodbye')
+
+// With Promise:
+// ============
+
 function setTimeoutPromiseFunction () {
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('hello')
-      resolve('goodbye')
+      // console.log('hello')
+      // resolve('goodbye')
       // reject('Oh no!')
     }, 0);
   })
 }
-////more stuff
 
 setTimeoutPromiseFunction().then((arg) => {
-  console.log(arg)
+  // console.log(arg)
 }).catch(err => {
-  console.log(err)
+  // console.log(err)
 })
-console.log('post promise')
+// console.log('post promise')
+
+// API call
+// ========
 function getCat() {
   axios.get('https://aws.random.cat/meow')
     .then((res) => {
-      document.body.innerHTML = '';
-      const el = document.createElement('img');
-      el.src = res.data.file;
-      el.style.height = '500px'
-      el.style.width = '500px'
-      document.body.appendChild(el)
+      const data = res.data;
+      appendCat(data)
     })
 }
-getCat();
-setInterval(getCat, 3000)
+// getCat();
+// setInterval(getCat, 3000)
+
+
+// API call with async / await
+// ===========================
+
+async function getAnotherCat() {
+  const { data } = await axios.get('https://aws.random.cat/meow')
+  appendCat(data);
+
+  // Same as below
+  // const res = await axios.get('https://aws.random.cat/meow')
+  // appendCat(res.data);
+}
+
+// getAnotherCat()
+
+// Helper
+// ======
+
+// Note destructure is happening at the argument level here.
+function appendCat({ file }) {
+  document.body.innerHTML = '';
+  const el = document.createElement('img');
+  el.src = file;
+  el.style.height = '500px'
+  el.style.width = '500px'
+  document.body.appendChild(el)
+}
